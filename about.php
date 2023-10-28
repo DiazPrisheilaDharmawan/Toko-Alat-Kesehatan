62ce783c847053d6558462af91ae83990a334a18
DiazPrisheilaDharmawan <diazdharmawan03@gmail.com>
2023-10-28 12:29:38 +0700

    first init github

---
+++ about.php
@@ -0,0 +1,48 @@
+<!-- Masthead-->
+<header class="masthead">
+    <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center text-center">
+            <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                <h2 class="text-white">About Us</h2>
+                <hr class="divider my-4" />
+            </div>
+
+        </div>
+    </div>
+</header>
+
+<section class="page-section">
+    <div class="container">
+        <!-- <?php echo html_entity_decode($_SESSION['setting_about_content']) ?> -->
+
+        <h2 style="font-size:28px;background: transparent; position: relative;">Selamat Datang di Toko Alat Kesehatan</h2>
+        <p style="text-align: justify; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">
+                Toko Alat Kesehatan menyediakan solusi lengkap bagi vendor dan juga pelanggan melalui satu pintu dengan 
+                memanfaatkan internet. Kami adalah penyedia terkemuka alat kesehatan dan peralatan medis berkualitas tinggi 
+                untuk solusi kesehatan terbaik anda. Toko alat kesehatan telah melayani pelanggan dengan penuh dedikasi dan 
+                komitmen untuk memastikan kesejahteraan dan kenyamanan pelanggan.
+            </font>
+        </p>
+        <p style="text-align: justify; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">
+                menyediakan produk-produk berkualitas tinggi yang telah teruji dan terpecaya, agar pelanggan mendapatkan 
+                yang terbaik untuk kesehatan. Tim kami siap membantu pelanggan dengan pengetahuan dan dukungan yang dibutuhkan.
+                Kami selalu siap untuk menjawab pertanyaan dan memberikan panduan terbaik. Toko alat kesehatan menawarkan 
+                beragam alat kesehatan dan peralatan medis, mulai dari monitoring kesehatan hingga perawatan kecantikan.
+                Toko kami mengutamakan pengiriman yang cepat dan handal, sehingga pelanggan dapat menerima pesanan dengan segera.
+                Kepuasan pelanggan adalah prioritas kami. Kami berkomitmen untuk membuat pengalaman belanja anda senyaman mungkin.
+            </font>
+        </p>
+        <p style="text-align: justify; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">
+                Terima kasih telah mempercayai kami sebagai mitra kesehatan anda. Kami berharap dapat membantu dalam mencapai
+                gaya hidup sehat dan bahagia. Jangan ragu untuk menghubungi kami, jika anda memiliki pertanyaan atau membutuhkan
+                bantuan. Kami siap melayani anda.
+            </font>
+        </p>
+        <p style="text-align: left; margin-bottom: 15px; padding: 0px;">
+            <font color="#000000" face="Open Sans, Arial, sans-serif">Salam Sehat, Toko Alat Kesehatan</font>
+        </p>
+    </div>
+</section>
